c661582e408358e5058d3c2163dc3b186172207a
bestzed14 <66513097+bestzed14@users.noreply.github.com>
2020-07-11 20:59:00 +0800

    移動

--- unity_0606run/Assets/scripts/Player.cs
+++ unity_0606run/Assets/scripts/Player.cs
@@ -20,13 +20,13 @@ public class Player : MonoBehaviour
      */
 
     #region 欄位區域
-    [Header("速度"),Tooltip("腳色的移動速度"),Range(50,150)]
-    public int speed=50;
+    [Header("速度"), Tooltip("腳色的移動速度"), Range(0, 15)]
+    public int speed = 10;
     [Tooltip("腳色血量")]
-    public float hp=999.9f;
+    public float hp = 999.9f;
     public int coin;
-    [Tooltip("跳躍高度")]
-    public int height;
+    [Tooltip("跳躍高度"),Range(50,500)]
+    public int height = 50;
     [Tooltip("跳躍音效")]
     public AudioClip soundjp;
     [Tooltip("滑行音效")]
@@ -37,9 +37,13 @@ public class Player : MonoBehaviour
     public bool liveOrDeath;
     [Tooltip("動畫控制器")]
     public Animator ani;
+    [Tooltip("碰撞器")]
+    public CapsuleCollider2D cc2d;
+    [Tooltip("剛體")]
+    public Rigidbody2D rig;
     #endregion
 
-    
+
     #region 方法區域
     /*
      * 摘要:方法的說明
@@ -48,9 +52,31 @@ public class Player : MonoBehaviour
     /// <summary>
     /// 腳色的跳躍功能:跳躍動畫+跳躍音效
     /// </summary>
+    public bool IsGround;
+    /// <summary>
+    /// 腳色移動
+    /// </summary>
+    private void move()
+    {
+        if (rig.velocity.magnitude<10) //velocity 加速度 
+        {
+        rig.AddForce(new Vector2(speed, 0));
+        }
+        
+    }
     private void Jump()
     {
-        print("jump啟動!!");
+        bool jump = Input.GetKey(KeyCode.Space);
+        ani.SetBool("jump switch", !IsGround);
+        if (IsGround)
+        {
+            if (jump)
+            {
+
+                IsGround = false;
+            rig.AddForce(new Vector2(0, height)); //剛體增加推力(二維向量)
+            }
+        }
     }
     /// <summary>
     /// 腳色的滑行功能:滑行動畫+滑行音效+縮小碰撞範圍
@@ -59,6 +85,18 @@ public class Player : MonoBehaviour
     {
         bool key = Input.GetKey(KeyCode.Z);
         ani.SetBool("slide switch", key);
+        if (key)
+        {
+            cc2d.offset = new Vector2(-0.9796966f, -0.97f);
+            cc2d.size = new Vector2(1.944118f, 1.91f);
+        }
+        else
+        {
+         cc2d.offset=new Vector2(-0.9796966f, 0.01935404f);
+         cc2d.size=new Vector2(1.944118f, 3.585742f);
+        }
+       
+
     }
     /// <summary>
     /// 吃金幣:金幣數量增加+金幣消失+金幣音效
@@ -88,18 +126,33 @@ public class Player : MonoBehaviour
     /*開始 start-遊戲開始時執行一次
      * 初始化之類的
      */
-    private void Start()
-    {
-       
-    }
+    
     /*
      * 更新 update
      * 開始遊戲後一秒執行約60次
      * 移動 監聽玩家鍵盤,滑鼠與觸控
      */
+    
     private void Update()
     {
         Slide();
+        
+    }
+    private void FixedUpdate()
+    {
+        Jump();
+        move();
     }
     #endregion
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板" || gameObject.name=="漂浮地板" || gameObject.name == "其他物件" || gameObject.name == "雜物")
+        {
+            //是否在地板
+            //搬家 選取後alt+上或下
+            //格式化 ctrl+ kd
+            IsGround = true;
+        }
+
+    }
 }
---
+++ unity_0606run/Assets/scripts/learnIf.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class learnIf : MonoBehaviour
+{
+    public bool test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (test)
+        {
+            print("123");
+        }
+    }
+    public bool opendoor;
+    // Update is called once per frame
+    void Update()
+    {
+        if (opendoor)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+    }
+}
